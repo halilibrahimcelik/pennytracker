83230c78c47891de21fef6b20d4bff51bb195f93
halilibrahimcelik <halil.celik139@gmail.com>
2025-10-25 21:12:14 +0100

    feat(transactions): add pagination component for transaction table; integrate dialog for delete confirmation

--- src/components/dashboard/transactions-table/colums.tsx
+++ src/components/dashboard/transactions-table/colums.tsx
@@ -10,7 +10,19 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-import { MoreHorizontal } from 'lucide-react';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { MoreHorizontal, DeleteIcon } from 'lucide-react';
+import { MdOutlineDeleteSweep } from 'react-icons/md';
+
 interface Transaction {
   id: string;
   amount: string;
@@ -34,29 +46,56 @@ export const columns: ColumnDef<Transaction>[] = [
   { accessorKey: 'category', header: 'Category' },
   {
     id: 'actions',
+    header: 'Actions',
     cell: ({ row }) => {
-      const payment = row.original;
+      const transaction = row.original;
 
       return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant='ghost' className='h-8 w-8 p-0'>
-              <span className='sr-only'>Open menu</span>
-              <MoreHorizontal className='h-4 w-4' />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align='end'>
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem
-              onClick={() => navigator.clipboard.writeText(payment.id)}
-            >
-              Copy payment ID
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>View customer</DropdownMenuItem>
-            <DropdownMenuItem>View payment details</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant='ghost' className='h-8 w-8 p-0'>
+                <span className='sr-only'>Open menu</span>
+                <MoreHorizontal className='h-4 w-4' />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align='end'>
+              <DropdownMenuItem className='w-full'>Details </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <Dialog>
+                <DialogTrigger asChild>
+                  <DropdownMenuItem
+                    onSelect={(e) => {
+                      e.preventDefault();
+                    }}
+                    title='Delete'
+                    variant='destructive'
+                  >
+                    <MdOutlineDeleteSweep className='text-danger' />
+                    Delete
+                  </DropdownMenuItem>
+                </DialogTrigger>
+                <DialogContent className='sm:max-w-[425px]'>
+                  <DialogHeader>
+                    <DialogTitle>
+                      Delete Transaction #{transaction.id}{' '}
+                    </DialogTitle>
+                    <DialogDescription>
+                      Are you sure you want to delete this transaction? This
+                      action cannot be undone.
+                    </DialogDescription>
+                  </DialogHeader>
+                  <DialogFooter>
+                    <DialogClose asChild>
+                      <Button variant='outline'>Cancel</Button>
+                    </DialogClose>
+                    <Button type='submit'>Yes</Button>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </>
       );
     },
   },
