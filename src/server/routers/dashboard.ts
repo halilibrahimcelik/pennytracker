6e2cbebe6a1b5160f758e6b3fb9f9d9c349bba54
halilibrahimcelik <halil.celik139@gmail.com>
2025-11-11 08:05:34 +0000

    feat(dashboard): add monthly flow query to calculate income and expenses by month

--- src/server/routers/dashboard.ts
+++ src/server/routers/dashboard.ts
@@ -1,6 +1,6 @@
 import { protectedProcedure, router } from '../trpc';
 import { and, eq, desc, gte, lte, sql } from 'drizzle-orm';
-import { transaction } from '@/db/schema';
+import { transaction, user } from '@/db/schema';
 import { db } from '@/db';
 
 import { z } from 'zod';
@@ -38,5 +38,44 @@ export const dashboardRouter = router({
         totalExpense: parseFloat(Number(row.totalExpense).toFixed(2)),
         net: parseFloat(net.toFixed(2)),
       };
+    }),
+  montlyFlow: protectedProcedure
+    .input(
+      z.object({
+        months: z.number().min(1).max(12).default(6),
+        to: z.coerce.date().optional(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const userId = ctx.session?.user.id!;
+      const toDate = input.to || new Date();
+      const from = new Date(toDate);
+      from.setMonth(from.getMonth() - (input?.months ?? 6) + 1);
+      const where = and(
+        eq(transaction.userId, userId),
+        gte(transaction.transactionDate, from),
+        lte(transaction.transactionDate, toDate)
+      );
+      const rows = await db
+        .select({
+          bucket: sql<string>`
+        to_char(date_trunc('month', ${transaction.transactionDate}), 'YYYY-MM')`,
+          totalIncome: sql<number>`
+        coalesce(sum(CASE WHEN ${transaction.transactionType} = 'income'
+          THEN (${transaction.amount})::numeric ELSE 0 END),0)`,
+          totalExpense: sql<number>`
+        coalesce(sum(CASE WHEN ${transaction.transactionType}= "expense"
+          THEN (${transaction.amount})::numeric ELSE 0 END),0
+        )`,
+        })
+        .from(transaction)
+        .where(where)
+        .groupBy(sql`date_trunc("month",${transaction.transactionDate})`)
+        .orderBy(sql`date_trunc("month",${transaction.transactionDate}) asc`);
+      return rows.map((row) => ({
+        month: row.bucket,
+        totalIncome: parseFloat(Number(row.totalIncome).toFixed(2)),
+        totalExpense: parseFloat(Number(row.totalExpense).toFixed(2)),
+      }));
     }),
 });
