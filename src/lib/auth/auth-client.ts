3db26d2266c04fa810c82ca44b2f096d0a921c3f
halilibrahimcelik <halil.celik139@gmail.com>
2025-11-18 22:53:52 +0000

    fix(auth-client): comment out baseURL to clarify its optional nature

--- src/lib/auth/auth-client.ts
+++ src/lib/auth/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from 'better-auth/react';
 const authClient = createAuthClient({
-  /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: process.env.BASE_URL!,
+  // /** The base URL of the server (optional if you're using the same domain) */
+  // baseURL: process.env.BASE_URL!,
 });
 
 export const { signIn, signOut, getSession, useSession, signUp } = authClient;
